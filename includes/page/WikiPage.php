523c0a9a63407671624324ec41cbdc8e2aa81502
Máté Szabó <mszabo@wikia-inc.com>
2021-01-04 14:35:38 +0100

    WikiPage: Add process cache for content model
    WikiPage::getContentModel() gets called several times per request via
    Action::getActionName(), triggering memcached I/O on each call. Excimer icicle
    graphs for index.php indicate about 1.26% of time is spent in this method, so
    let's add process caching to avoid redundant memcached fetches on subsequent
    invocations.
    
    Change-Id: Icdb9ffb849a0a0a264083957431495f8f4fb783e

--- includes/page/WikiPage.php
+++ includes/page/WikiPage.php
@@ -662,7 +662,8 @@ class WikiPage implements Page, IDBAccessObject {
 						wfWarn( "Page $title exists but has no (visible) revisions!" );
 						return $this->mTitle->getContentModel();
 					}
-				}
+				},
+				[ 'pcTTL' => $cache::TTL_PROC_LONG ]
 			);
 		}
 
