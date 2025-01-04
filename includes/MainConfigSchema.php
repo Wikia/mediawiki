7ae8896fa599ec319f370cc4152fcb84962104ef
Reedy <reedy@wikimedia.org>
2025-01-04 23:52:17 +0000

    DnsBlacklistUrls: Remove sorbs.net
    Service is no longer running, so it's a default that doesn't do anything
    
    Bug: T382987
    Change-Id: I3a21c12ba689928d38e410cbe2547ab7e616ac8a
    (cherry picked from commit 4a6fac8b7dadfdffe6e0e239b8a551436e147d97)

--- includes/MainConfigSchema.php
+++ includes/MainConfigSchema.php
@@ -8676,7 +8676,7 @@ class MainConfigSchema {
 	 * @since 1.16
 	 */
 	public const DnsBlacklistUrls = [
-		'default' => [ 'http.dnsbl.sorbs.net.', ],
+		'default' => [],
 		'type' => 'list',
 	];
 
